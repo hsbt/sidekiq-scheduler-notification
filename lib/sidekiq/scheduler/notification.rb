e5237d580cfaa4ae83fefeb00f53197dd27a3ed9
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2016-07-23 19:27:01 +0900

    autoload for notification middleware

--- lib/sidekiq/scheduler/notification.rb
+++ lib/sidekiq/scheduler/notification.rb
@@ -28,3 +28,9 @@ module SidekiqSchedulerNotification
 end
 
 Sidekiq::Scheduler.prepend SidekiqSchedulerNotification
+
+Sidekiq.configure_server do |config|
+  config.server_middleware do |chain|
+    chain.add Sidekiq::Middleware::Server::SlackNotification
+  end
+end
